9614e4d71cdd94e1c0ebecd6cf51745c839f99da
Nicolas Camargo <nicofcamon@gmail.com>
2021-07-18 00:53:38 +0200

    Adding the first script

---
+++ test-script.R
@@ -0,0 +1,14 @@
+################
+# TEST SCRIPTS #
+# IN PROJECTS  #
+################
+
+#' * This is just for testing purposes to save within the project*
+
+###
+# LIBRARIES
+###
+
+library(tidyverse)
+library(dslabs)
+

f8cd40f5b9028382b91928f99e20363a8b3280a9
Nicolas Camargo <nicofcamon@gmail.com>
2021-07-18 00:55:58 +0200

    Add Comments about Git

--- test-script.R
+++ test-script.R
@@ -12,3 +12,12 @@
 library(tidyverse)
 library(dslabs)
 
+
+#' * In this script that goes to GITHub we can see the git tab next to the environment *
+  #' [ Files in yellow refer to files that github has no clue of] 
+  #' [ When working with existing repositories the first thing to do is to PULL the 
+  #' [ data and scripts inside the repository ]
+  #' [ We can click on the boxes of the files we want to COMMIT into the repository]
+  #' [ Note that not all files have to be committed - some can be ignored]
+  #' [ Files committed need to be pushed into the repositories after]
+   
